cc9ee0b56f70501dc4c43a8a5de84cb342710e77
Antonio <75948828+AntonioMontenegro-Glich@users.noreply.github.com>
2024-10-03 09:01:22 -0300

    Atualizando professorRoutes.js

--- routes/professorRoutes.js
+++ routes/professorRoutes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const professorController = require('../controllers/professorController');
 
-
+//rotas professor
+router.get('/getAll', professorController.getAllProfessores);
 router.post('/create', professorController.createProfessor);
 
 module.exports = router;
