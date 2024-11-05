ec5c20d9fe5867b92647dfde3b9e6616069baa60
AntonioMontenegro-Glich <antoniolemos2004@gmail.com>
2024-11-05 16:21:03 -0300

    Organizando jsonwebtoken 1° parte

--- routes/avisoRoutes.js
+++ routes/avisoRoutes.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const avisoController = require("../controllers/avisoController"); 
+const autenticaMiddlewareProfessor = require("../middleware/autenticaMiddlewareProfessor");
 
 //Rotas dos Avisos
 
 // Criar aviso de Professor
-router.post("/aviso", avisoController.createAviso);
+router.post("/aviso", autenticaMiddlewareProfessor, avisoController.createAviso);
 
 // Criar aviso de Coordenador (para todas as turmas)
-router.post("/aviso/coordenador", avisoController.createAvisoCoordenador);
+router.post("/aviso/coordenador", autenticaMiddlewareProfessor, avisoController.createAvisoCoordenador);
 
 // Atualizar aviso
-router.put("/aviso/:id", avisoController.updateAviso);
+router.put("/aviso/:id", autenticaMiddlewareProfessor, avisoController.updateAviso);
 
 // Obter todos os avisos do autor logado
 router.get("/aviso", avisoController.getAllAvisos);
